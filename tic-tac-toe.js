d1cdd71f16a9d95e0c9e8899e0d5e99626977a60
bailey <paulbaileyuni@gmail.com>
2023-10-21 21:27:16 -0800

    Added Exercise 4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -152,3 +152,84 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+// Exercise 4
+document.addEventListener("DOMContentLoaded", function () {
+    const squares = document.querySelectorAll("#board div");
+    const status = document.getElementById("status");
+    const newGameButton = document.querySelector(".btn");
+
+    let currentPlayer = "X";
+    let gameBoard = ["", "", "", "", "", "", "", "", ""];
+
+    function checkWinner() {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (const combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+                status.classList.add("you-won");
+                status.textContent = `Congratulations! ${gameBoard[a]} is the Winner!`;
+                return true;
+            }
+        }
+
+        if (gameBoard.every((square) => square !== "")) {
+            status.textContent = "It's a draw!";
+            return true;
+        }
+
+        return false;
+    }
+
+    function handleSquareClick(event) {
+        const square = event.target;
+        const index = Array.from(squares).indexOf(square);
+
+        if (gameBoard[index] === "") {
+            gameBoard[index] = currentPlayer;
+            square.textContent = currentPlayer;
+            square.classList.add(currentPlayer);
+
+            if (!checkWinner()) {
+                currentPlayer = currentPlayer === "X" ? "O" : "X";
+                status.classList.remove("you-won");
+                status.textContent = `Player ${currentPlayer}'s turn`;
+            }
+        }
+    }
+
+    function handleSquareHover(event) {
+        const square = event.target;
+        square.classList.add("hover");
+        square.addEventListener("mouseout", function () {
+            square.classList.remove("hover");
+        });
+    }
+
+    squares.forEach((square) => {
+        square.addEventListener("click", handleSquareClick);
+        square.addEventListener("mouseover", handleSquareHover);
+    });
+
+    newGameButton.addEventListener("click", function () {
+        gameBoard = ["", "", "", "", "", "", "", "", ""];
+        status.textContent = "Move your mouse over a square and click to play an X or an O.";
+        status.classList.remove("you-won");
+        currentPlayer = "X";
+        squares.forEach((square) => {
+            square.textContent = "";
+            square.classList.remove("X", "O");
+        });
+    });
+});
+
